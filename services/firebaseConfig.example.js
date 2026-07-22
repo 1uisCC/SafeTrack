947261ec7a2541640e894ec9d5915920110d4e7b
Jose luis Cortes Cruz <josecortescruz540@gmail.com>
2026-07-22 00:16:39 -0600

    Quitar credenciales reales del archivo example

--- services/firebaseConfig.example.js
+++ services/firebaseConfig.example.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDIHVxd661-dR7y01kHJ7NtLwqvHZmG6Tg",
-  authDomain: "safetrack-4aa9e.firebaseapp.com",
-  projectId: "safetrack-4aa9e",
-  storageBucket: "safetrack-4aa9e.appspot.com",
-  messagingSenderId: "366420393530",
-  appId: "1:366420393530:android:eb972a4df63e556e797f03"
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID_AQUI",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "TU_SENDER_ID_AQUI",
+  appId: "TU_APP_ID_AQUI"
 };
